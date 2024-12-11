cc2068a1de63bc44726308dceec35f755c6d797d
Admin <Admin@DESKTOP-ARHUM8Q>
2024-12-11 15:23:14 +0700

    Add project files.

---
+++ ThuVienWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThuVienWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
